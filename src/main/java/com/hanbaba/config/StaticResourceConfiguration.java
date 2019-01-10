392a568e76b6b275b6c62feadea57285b64cb5f3
raz.rogek <raz.rogek@imperva.com>
2019-01-10 14:07:59 +0200

    allowing CORS

--- src/main/java/com/hanbaba/config/StaticResourceConfiguration.java
+++ src/main/java/com/hanbaba/config/StaticResourceConfiguration.java
@@ -1,8 +1,11 @@
 package com.hanbaba.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class StaticResourceConfiguration implements WebMvcConfigurer {
@@ -11,4 +14,15 @@ public class StaticResourceConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/swagger").setViewName("redirect:/swagger-ui.html");
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api").allowedOrigins("http://localhost");
+            }
+        };
+    }
+
 }
